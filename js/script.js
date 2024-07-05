b92d0d382651c176e9f8d0d7df7153a43c6231c6
Melissa Harrison <78873771+melissaahh@users.noreply.github.com>
2024-07-04 21:47:32 -0400

    Added async function

--- js/script.js
+++ js/script.js
@@ -7,8 +7,21 @@ const eightGuesses = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
-const word = "magnolia";
+let word = "magnolia";
 const guessedLetters = [];
+let remainingGuesses = 8;
+
+const getWord = async function (){
+    const response = await fetch("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
+    const words = await response.text();
+    const wordArray = words.split("\n");
+    const randomIndex = math.floor(math.random() * wordArray.length);
+    word = wordArray[randomIndex].trim();
+    placeholder(word);
+}
+
+getWord();
+
 
 // add placeholders functions
 
@@ -21,7 +34,6 @@ const placeholder = function (word){
     wordInProgress.innerText = placeholderLetters.join("");
 }; 
 
-placeholder(word);
 
 
 guessButton.addEventListener("click", function(e){
@@ -85,6 +97,24 @@ const updateWordInProgress = function (guessedLetters){
     
     wordInProgress.innerText = revealWord.join("");
     checkIfWin();
+};
+
+const updateGuessesRemaining = function (guess){
+    const upperWord = word.toUpperCase();
+    if (!upperWord.includes(guess)){
+        message.innerTet = `Sorry, the word has no ${guess}.`;
+        remainingGuesses -= 1;
+    } else {
+        message.innerText = `Good guess! The word has the letter ${guess}.`;
+    }
+
+    if (remainingGuesses === 0){
+        message.innerHTML =  `Game over! The word was <span class="highlight">${word}</span>.`;
+    } else if (remainingGuesses === 1){
+        remainingGuessesSpan.innerText = `${remainingGuesses} guess`;
+    } else {
+        remainingGuessesSpan.innerText = `${remainingGuesses} guesses`;
+    }
 };
 
 const checkIfWin = function (){
